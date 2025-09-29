b5aeee8619c03042612b80d1d2641740d18b880f
M-For-Mahathi <163542249+M-For-Mahathi@users.noreply.github.com>
2025-09-29 21:17:40 +0530

    Time: 6 ms (9.29%), Space: 41.4 MB (52.01%) - LeetHub

---
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minScoreTriangulation(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+
+        for (int gap = 2; gap < n; gap++) {
+            for (int i = 0; i + gap < n; i++) {
+                int j = i + gap;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++) {
+                    int cost = dp[i][k] + dp[k][j] + values[i] * values[k] * values[j];
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
