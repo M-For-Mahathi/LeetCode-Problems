ba84160ebbdccf3ea4c94c10b583baf6820285c0
M-For-Mahathi <163542249+M-For-Mahathi@users.noreply.github.com>
2026-06-17 07:50:45 +0530

    Time: 42 ms (8.92%), Space: 42.1 MB (51.23%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {        
+    HashMap<Integer, Integer> memo = new HashMap<>();
+
+    public int fib(int n) {
+
+        if (n <= 1) return n;
+
+        if (memo.containsKey(n)) return memo.get(n);
+
+        int ans = fib(n-1) + fib(n-2);
+
+        memo.put(n,ans);
+
+        return ans;
+    }
+}
